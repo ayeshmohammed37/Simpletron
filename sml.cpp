67d980bcdae786c8de1741d59aec5c9cd0d8b874
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-02-23 20:26:54 +0200

    Input/Output operations

--- sml.cpp
+++ sml.cpp
@@ -1,43 +1,56 @@
 #include <array>
+#include <cstdlib>
 #include <iostream>
 #include <print>
 #include <span>
 
+constexpr size_t size{100};
+
+enum class OperationCodes {
+  // Input/Output operations
+  read = 10,
+  write = 11,
+  
+  // Load and Store Operations
+  load = 20,
+  store = 21,
+  
+  // Arithmetic operations
+  add = 30,
+  subtract = 31,
+  divide = 32,
+  multiply = 33,
+  
+  // Transfare-of-control operations
+  branch = 40,
+  branchNeg = 41,
+  branchZero = 42,
+  halt = 43
+};
+
 void getSMLProgram(std::span<int>);
+OperationCodes getOperationType(int&);
 void Read(int&);
 
+
 int main() {
-  constexpr size_t size{100};
-  enum class OperationCodes {
-    // Input/Output operations
-    read = 10,
-    write = 11,
-    
-    // Load and Store Operations
-    load = 20,
-    store = 21,
-    
-    // Arithmetic operations
-    add = 30,
-    subtract = 31,
-    divide = 32,
-    multiply = 33,
-    
-    // Transfare-of-control operations
-    branch = 40,
-    branchNeg = 41,
-    branchZero = 42,
-    halt = 43
-  };
   
   int accumulator{0};
   std::array<int, size> memory{};
   
   getSMLProgram(memory);
   
-  for (size_t i{0}; i < size && memory.at(i) != 0; ++i) {
-    // Input/Output operations
+  std::print("\nStart runing Program from memory....\n");
 
+  for (size_t i{0}; i < size && memory.at(i) != 0; ++i) {
+    OperationCodes operation{getOperationType(memory.at(i))};
+
+    switch (operation) {
+      case OperationCodes::read:
+
+    }
+    // Input/Output operations
+    
     // Load and Store Operations
     // Arithmetic operations
     // Transfare-of-control operations
@@ -63,6 +76,47 @@ void getSMLProgram(std::span<int> memory) {
   std::println();
 }
 
-void Read(int& address) {
-  
+OperationCodes getOperationType(int& instruction) {
+  int code{std::abs(instruction / 100)};
+
+  switch (code) {
+    case 10:
+      return OperationCodes::read;
+    case 11:
+      return OperationCodes::write;
+    case 20:
+      return OperationCodes::load;
+    case 21:
+      return OperationCodes::store;
+    case 30:
+      return OperationCodes::add;
+    case 31:
+      return OperationCodes::subtract;
+    case 32:
+      return OperationCodes::divide;
+    case 33:
+      return OperationCodes::multiply;
+    case 40:
+      return OperationCodes::branch;
+    case 41:
+      return OperationCodes::branchNeg;
+    case 42:
+      return OperationCodes::branchZero;
+    case 23:
+      return OperationCodes::halt;    
+  }
 }
+
+void Read(std::array<int, size>& memory, int location) {
+  int number;
+  
+  std::print("Number: ");
+  std::cin >> number;
+
+  memory.at(location) = number;
+}
+
+void write(std::array<int, size>& memory, int location) {
+  std::println("{}", memory.at(location));
+}
+
