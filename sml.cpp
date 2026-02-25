2c74b5d470ff04deea7f2f818cdd6006d1913726
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-02-25 04:44:46 +0200

    getProgram(): implementation

--- sml.cpp
+++ sml.cpp
@@ -1,7 +1,5 @@
 /*
-Redesign SML:
-- Initialize memory_size{100}
-- Initialize memory[memory_size]
+
 - Input SML program or 11111 to end
     - initialize i{0}
     - while i < memory_size
@@ -18,186 +16,38 @@ Redesign SML:
 */
 
 #include <array>
-#include <cstdlib>
 #include <iostream>
 #include <print>
-#include <span>
 
-constexpr size_t size{100};
+const int memory_size{100};
 
-enum class OperationCodes {
-  // Input/Output operations
-  read = 10,
-  write = 11,
-  
-  // Load and Store Operations
-  load = 20,
-  store = 21,
-  
-  // Arithmetic operations
-  add = 30,
-  subtract = 31,
-  divide = 32,
-  multiply = 33,
-  
-  // Transfare-of-control operations
-  branch = 40,
-  branchNeg = 41,
-  branchZero = 42,
-  halt = 43
-};
-
-void getSMLProgram(std::span<int>);
-OperationCodes getOperationType(int&);
-int getLocation(const int&);
-
-// Input/Output operations
-// read data from keyboard to a specific memory location
-void read(std::array<int, size>&, int);
-// write data from specific memory location to the screen
-void write(const std::array<int, size>&, int);
-
-// Load/Store Operations
-// load data from memory into the accumulator
-void load(const std::array<int, size>&, int, int&);
-// store data from accumulator into specific memory location
-void store(std::array<int, size>&, int, const int&);
-
-//Arithmeticc operations
-// add data from specific memory location to accumulator 
-void add(const std::array<int, size>&, int, int&);
-// subtract data from specific memory location from accumulator 
-void subtract(const std::array<int, size>&, int, int&);
-// divide data from specific memory location into accumulator 
-void divide(const std::array<int, size>&, int, int&);
-// multiply data from specific memory location to accumulator 
-void multiply(const std::array<int, size>&, int, int&);
-
+// get sml program into memory
+void getProgram(std::array<int, memory_size>&);
 
 int main() {
-  std::array<int, size> memory{};
-  
-  getSMLProgram(memory);
-  
-  std::print("\nStart runing Program from memory....\n");
-  
-  int accumulator{0};
-  int pointer{0};
-  int instruction{memory.at(0)};
-  OperationCodes operation{getOperationType(instruction)};
-  int location{getLocation(instruction)};
+  std::array<int, memory_size> memory{0};
 
-  while (operation != OperationCodes::halt && pointer < size) {
-    switch (operation) {
-      case OperationCodes::read:
-        read(memory, location);
-        pointer++;
-        instruction = memory.at(pointer);
-        operation = getOperationType(instruction);
-        location = getLocation(instruction);
-        break;
-      
-      case OperationCodes::write:
-        write(memory, location);
-        ++pointer;
-        instruction = memory.at(pointer);
-        operation = getOperationType(instruction);
-        location = getLocation(instruction);
-        break;
-
-      
-    }
-  }
-  
+  getProgram(memory);
 }
 
+void getProgram(std::array<int, memory_size>& memory) {
+  std::println("Enter SML Program or 11111 to end program.");
 
-void getSMLProgram(std::span<int> memory) {
-  std::cout << "Enter SML program or 11111 to exit:\n";
-  
-  for (int input; int& address : memory) {
-    std::cout << ">";
-    std::cin >> input;
-    
-    if (input == 11111) {
-      return;
+  int instruction;
+  size_t i{0};
+  while (i < memory_size) {
+    std::print("> ");
+    std::cin >> instruction;
+
+    if (instruction == 11'111) {
+      break;
     }
 
-    address = input;
-  }
+    if (instruction > 9'999 || instruction < -9'999) {
+      std::println("Error: instruction not valid.");
+      continue;
+    }
 
-  std::println();
-}
-
-OperationCodes getOperationType(int& instruction) {
-  int code{std::abs(instruction / 100)};
-
-  switch (code) {
-    case 10:
-      return OperationCodes::read;
-    case 11:
-      return OperationCodes::write;
-    case 20:
-      return OperationCodes::load;
-    case 21:
-      return OperationCodes::store;
-    case 30:
-      return OperationCodes::add;
-    case 31:
-      return OperationCodes::subtract;
-    case 32:
-      return OperationCodes::divide;
-    case 33:
-      return OperationCodes::multiply;
-    case 40:
-      return OperationCodes::branch;
-    case 41:
-      return OperationCodes::branchNeg;
-    case 42:
-      return OperationCodes::branchZero;
-    case 23:
-      return OperationCodes::halt;    
+    memory.at(i++) = instruction;
   }
 }
-
-int getLocation(const int& instruction) {
-  return std::abs(instruction % 100);
-}
-
-void read(std::array<int, size>& memory, int location) {
-  int number;
-  
-  std::print("Number: ");
-  std::cin >> number;
-
-  memory.at(location) = number;
-}
-
-void write(const std::array<int, size>& memory, int location) {
-  std::println("{}", memory.at(location));
-}
-
-void load(const std::array<int, size>& memory, int location, int& accumulator) {
-  accumulator = memory.at(location);
-}
-
-void store(std::array<int, size>& memory, int location, const int& accumulator) {
-  memory.at(location) = accumulator;
-}
-
-void add(const std::array<int, size>& memory, int location, int& accumulator) {
-  accumulator += memory.at(location);
-}
-
-void subtract(const std::array<int, size>& memory, int location, int& accumulator) {
-  accumulator -= memory.at(location);
-}
-
-void divide(const std::array<int, size>& memory, int location, int& accumulator) {
-  accumulator /= memory.at(location);
-}
-
-void multiply(const std::array<int, size>& memory, int location, int& accumulator) {
-  accumulator *= memory.at(location);
-}
-
