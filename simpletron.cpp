da9c381fbea51c6fefd6d7fed79c014a8bc2be32
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-03-07 04:30:43 +0200

    sml load program into memory

--- simpletron.cpp
+++ simpletron.cpp
@@ -16,151 +16,148 @@ enum class OperationCodes {
   branch = 40, branchNeg, branchZero, halt
 };
 
-const std::string startingMsg{
-  "***           Welcome to Simpletron           ***\n"
-  "***                                           ***\n"
-  "*** Please enter your program one instruction ***\n"
-  "*** (or data word) at a time. I will type the ***\n"
-  "*** location number and a question mark (?).  ***\n"
-  "*** You then type the word for that location. ***\n"
-  "*** Type the sentinel -99999 to stop entering ***\n"
-  "*** your program.                             ***"
-};
-
-const std::string runningMsg{
-  "*** Program loading completed ***\n"
-  "*** Program execution begins  ***"
-};
-// reads the SML instructions from the user
+// load program into memory
 void load(std::span<int>);
 
 // execute sml program stored in memory
-void executeProgram(std::array<int, memory_size>&);
+// void executeProgram(std::array<int, memory_size>&);
 
 int main() {  
   std::array<int, memory_size> memory{0};
 
   load(memory);
-  executeProgram(memory);
+  
+  // executeProgram(memory);
 }
 
-inline void displayMsg(const std::string msg) {
-  std::print("{}\n", msg);
-}
+bool isValid(int instruction) {
+  int op{abs(instruction / 100)};
+  int location{abs(instruction % 100)};
 
+  bool opCheck{false};
+  bool locationCheck{false};
+
+  if (op == 10 || op == 11 || op == 20 || op == 21 || (op >= 30 && op <= 33) || (op >= 40 && op <= 43)) {
+    opCheck = true;
+  }
+  
+  if (location >= 0 && location < 100) {
+    locationCheck = true;
+  }
+
+  return opCheck && locationCheck;
+}
 
 void load(std::span<int> memory) {
-  displayMsg(startingMsg);
+  int ins{0};
+  int cnt{0};
 
-  int instruction;
-  size_t i{0};
-  while (i < memory_size) {
-    std::print("{:02d} ? ", i);
-    std::cin >> instruction;
+  while (cnt < memory.size()) {
+    std::print("{:02} ? ", cnt);
+    std::cin >> ins;
 
-    if (instruction =-99999) {
-      break;
+    if (ins > 9999 || ins < -9999) {
+      return;
     }
 
-    if (instruction > 9'999 || instruction < -9'999) {
-      std::println("Error: instruction not valid.");
+    if (!isValid(ins)) {
+      std::println("Error: the instruction is not valid.");
       continue;
     }
-
-    memory.at(i++) = instruction;
+    
+    memory.at(cnt) = ins;
+    cnt++;
   }
-
-  std::println();
 }
 
 
 
-void executeProgram(std::array<int, memory_size>& memory) {
-  // determine location from instruction 
-  auto getLocation{
-    [](const int& ins) { return std::abs(ins % 100);}
-  };
+// void executeProgram(std::array<int, memory_size>& memory) {
+//   // determine location from instruction 
+//   auto getLocation{
+//     [](const int& ins) { return std::abs(ins % 100);}
+//   };
 
-  // determine operation from instruction
-  auto getOperationCode{
-    [](const int& ins) { return static_cast<OperationCodes>(std::abs(ins / 100));}
-  };
+//   // determine operation from instruction
+//   auto getOperationCode{
+//     [](const int& ins) { return static_cast<OperationCodes>(std::abs(ins / 100));}
+//   };
 
-  std::println("*** Program loading completed ***");
-  std::println("*** Program execution begins  ***");
+//   std::println("*** Program loading completed ***");
+//   std::println("*** Program execution begins  ***");
 
-  int accumulator{0};
-  size_t memory_add{0};
+//   int accumulator{0};
+//   size_t memory_add{0};
 
-  while (memory_add < memory_size) {
-    int instruction{memory.at(memory_add)};
-    int location{getLocation(instruction)};
-    OperationCodes code{getOperationCode(instruction)};
+//   while (memory_add < memory_size) {
+//     int instruction{memory.at(memory_add)};
+//     int location{getLocation(instruction)};
+//     OperationCodes code{getOperationCode(instruction)};
 
-    // Input/Output operations
-    if (code == OperationCodes::read) {
-      int number;
-      std::print("? ");
-      std::cin >> number;
-      memory.at(location) = number;
-      memory_add++;
-    }
-    else if (code == OperationCodes::write) {
-      std::println("? {}", memory.at(location));
-      memory_add++;
-    }
-    // load/store operations
-    else if (code == OperationCodes::load) {
-      accumulator = memory.at(location);
-      memory_add++;
-    }
-    else if (code == OperationCodes::store) {
-      memory.at(location) = accumulator;
-      memory_add++;
-    }
-    // Arithmatic operations
-    else if (code == OperationCodes::add) {
-      accumulator += memory.at(location);
-      memory_add++;
-    }
-    else if (code == OperationCodes::subtract) {
-      accumulator -= memory.at(location);
-      memory_add++;
-    }
-    else if (code == OperationCodes::divide) {
-      accumulator *= memory.at(location);
-      memory_add++;
-    }
-    else if (code == OperationCodes::multiply) {
-      accumulator *= memory.at(location);
-      memory_add++;
-    }
-    // Transfer-of-control operations
-    else if (code == OperationCodes::branch) {
-      memory_add = location;
-    }
-    else if (code == OperationCodes::branchNeg) {
-      if (accumulator < 0) {
-        memory_add = location;
-      }
-      else {
-        memory_add++;
-      }
-    }
-    else if (code == OperationCodes::branchZero) {
-      if (accumulator == 0) {
-        memory_add = location;
-      }
-      else {
-        memory_add++;
-      }
-    }
-    else if (code == OperationCodes::halt) {
-      memory_add = 101;
-    }
-  }
+//     // Input/Output operations
+//     if (code == OperationCodes::read) {
+//       int number;
+//       std::print("? ");
+//       std::cin >> number;
+//       memory.at(location) = number;
+//       memory_add++;
+//     }
+//     else if (code == OperationCodes::write) {
+//       std::println("? {}", memory.at(location));
+//       memory_add++;
+//     }
+//     // load/store operations
+//     else if (code == OperationCodes::load) {
+//       accumulator = memory.at(location);
+//       memory_add++;
+//     }
+//     else if (code == OperationCodes::store) {
+//       memory.at(location) = accumulator;
+//       memory_add++;
+//     }
+//     // Arithmatic operations
+//     else if (code == OperationCodes::add) {
+//       accumulator += memory.at(location);
+//       memory_add++;
+//     }
+//     else if (code == OperationCodes::subtract) {
+//       accumulator -= memory.at(location);
+//       memory_add++;
+//     }
+//     else if (code == OperationCodes::divide) {
+//       accumulator *= memory.at(location);
+//       memory_add++;
+//     }
+//     else if (code == OperationCodes::multiply) {
+//       accumulator *= memory.at(location);
+//       memory_add++;
+//     }
+//     // Transfer-of-control operations
+//     else if (code == OperationCodes::branch) {
+//       memory_add = location;
+//     }
+//     else if (code == OperationCodes::branchNeg) {
+//       if (accumulator < 0) {
+//         memory_add = location;
+//       }
+//       else {
+//         memory_add++;
+//       }
+//     }
+//     else if (code == OperationCodes::branchZero) {
+//       if (accumulator == 0) {
+//         memory_add = location;
+//       }
+//       else {
+//         memory_add++;
+//       }
+//     }
+//     else if (code == OperationCodes::halt) {
+//       memory_add = 101;
+//     }
+//   }
 
-  std::println();
-  std::println("*** Simpletron execution terminated ***");
-}
+//   std::println();
+//   std::println("*** Simpletron execution terminated ***");
+// }
 
