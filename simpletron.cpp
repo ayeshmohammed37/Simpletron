e5f40901d5cb3c1839fd157c51572b271a9d1e58
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-03-08 06:35:00 +0200

    instruction execution cycle

--- simpletron.cpp
+++ simpletron.cpp
@@ -5,6 +5,7 @@
 #include <span>
 #include <string>
 
+
 // operation code specifying the operation to perform
 enum class OperationCodes {
   read = 10, write,
@@ -17,7 +18,7 @@ enum class OperationCodes {
 void load(std::span<int>);
 
 // execute sml program stored in memory
-void executeProgram(std::span<int>, int&, int&, int&, int&, int&);
+void executeProgram(std::span<int>, int&, int&, int&, OperationCodes&, int&);
 
 int main() {  
   // memory size: 100-word
@@ -30,7 +31,7 @@ int main() {
   int accumulator{0};
   int instructionCounter{0};
   int instructionRegister{0};
-  int operationCode{0};
+  OperationCodes operationCode{0};
   int operand{0};
 
   // print welcoming msg
@@ -76,9 +77,13 @@ void load(std::span<int> memory) {
   }
 }
 
-void executeProgram(std::span<int>, int& accumulator, int& instruction, int&, int&, int&) {
+
+
+void executeProgram(std::span<int> memory, int& accumulator,
+   int& instructionCounter, int& instructionRegister, OperationCodes& operationCode, int& operand) {
+
   // determine location from instruction 
-  auto getLocation{
+  auto getOperand{
     [](const int& ins) { return abs(ins % 100);}
   };
 
@@ -87,84 +92,116 @@ void executeProgram(std::span<int>, int& accumulator, int& instruction, int&, in
     [](const int& ins) { return static_cast<OperationCodes>(abs(ins / 100));}
   };
 
-  std::println("*** Program loading completed ***");
-  std::println("*** Program execution begins  ***");
-
-  int accumulator{0};
-  size_t memory_add{0};
-  int instruction{0};
-  OperationCodes operand;
-  int location{0};
-
-  while (memory_add < memory_size) {
-    instruction = memory.at(memory_add);
-    operand = getOperationCode(instruction);
-    location = getLocation(instruction);
+  // instruction execution cycle
+  while (instructionCounter < memory.size()) {
+    // fetch instruction from memory and store it in instructionRegister
+    instructionRegister = memory.at(instructionCounter);
     
-    // Input/Output operations
-    if (operand == OperationCodes::read) {
-      int number;
-      std::print("Number ? ");
-      std::cin >> number;
-      memory.at(location) = number;
-      memory_add++;
-    }
-    else if (operand == OperationCodes::write) {
-      std::println("? {}", memory.at(location));
-      memory_add++;
-    }
-    // load/store operations
-    else if (operand == OperationCodes::load) {
-      accumulator = memory.at(location);
-      memory_add++;
-    }
-    else if (operand == OperationCodes::store) {
-      memory.at(location) = accumulator;
-      memory_add++;
-    }
-    // Arithmatic operations
-    else if (operand == OperationCodes::add) {
-      accumulator += memory.at(location);
-      memory_add++;
-    }
-    else if (operand == OperationCodes::subtract) {
-      accumulator -= memory.at(location);
-      memory_add++;
-    }
-    else if (operand == OperationCodes::divide) {
-      accumulator /= memory.at(location);
-      memory_add++;
-    }
-    else if (operand == OperationCodes::multiply) {
-      accumulator *= memory.at(location);
-      memory_add++;
-    }
-    // Transfer-of-control operations
-    else if (operand == OperationCodes::branch) {
-      memory_add = location;
-    }
-    else if (operand == OperationCodes::branchNeg) {
-      if (accumulator < 0) {
-        memory_add = location;
-      }
-      else {
-        memory_add++;
-      }
-    }
-    else if (operand == OperationCodes::branchZero) {
-      if (accumulator == 0) {
-        memory_add = location;
-      }
-      else {
-        memory_add++;
-      }
-    }
-    else {
-      memory_add = 101;
-    }
-  }
+    // extract operationCode and operand from instruction register
+    operationCode = getOperationCode(instructionRegister); 
+    operand = getOperand(instructionRegister);
+    
+    // determine the operation code
+    switch (operationCode) {
+      // execute operation
+      // update instructionCounter
+      
+      // Input/output operations:
+      // read a word from keyboard into a specific memory location
+      case OperationCodes::read:
+        int number;
+        std::cout << "? ";
+        std::cin >> number;
+        memory.at(operand) = number;
+        instructionCounter++;
+        break;
 
-  std::println();
-  std::println("*** Simpletron execution terminated ***");
+      // write a word from a specific memory location to the screen
+      case OperationCodes::write:
+        std::cout << memory.at(operand);
+        instructionCounter++;
+        break;
+      
+      // Load/store operations
+      // Load a word from a specific memory location into the accumulator.
+      case OperationCodes::load:
+        accumulator = memory.at(operand);
+        instructionCounter++;
+        break;
+      
+      // Store a word from the accumulator into a specific memory location.
+      case OperationCodes::store:
+        memory.at(operand) = accumulator;
+        instructionCounter++;
+        break;
+      
+      // Arithmetic operations
+      // Add a word from a specific memory location to the word in the 
+      // accumulator (leave the result in the accumulator).
+      case OperationCodes::add:
+        accumulator += memory.at(operand);
+        instructionCounter++;
+        break;
+      
+      // Subtract a word from a specific memory location from the word in the
+      // accumulator (leave the result in the accumulator). 
+      case   OperationCodes::subtract:
+        accumulator -= memory.at(operand);
+        instructionCounter++;
+        break;
+    
+      // Divide a word from a specific memory location into the word in the
+      // accumulator (leave the result in the accumulator). 
+      case   OperationCodes::divide:
+        accumulator /= memory.at(operand);
+        instructionCounter++;
+        break;
+    
+      // Multiply a word from a specific memory location by the word in the
+      // accumulator (leave the result in the accumulator).
+      case   OperationCodes::multiply:
+        accumulator *= memory.at(operand);
+        instructionCounter++;
+        break;
+      
+      // Transfare-of-control operations:
+      // Branch to a specific memory location.
+      case OperationCodes::branch:
+        instructionCounter = operand;
+        break;
+      
+      // Branch to a specific memory location if the accumulator is negative.
+      case OperationCodes::branchNeg:
+        // check if accumulator is negative then change branch
+        switch (bool isNegative = accumulator < 0; isNegative) {
+          case true:
+            instructionCounter = operand;
+            break;
+          default:
+            instructionCounter++;
+            break;          
+        }
+        break;
+      
+      // Branch to a specific memory location if the accumulator is zero.
+      case OperationCodes::branchZero:
+        // check if accumulator is zero then change branch
+        switch (bool isZero = accumulator == 0; isZero) {
+            case true:
+              instructionCounter = operand;
+              break;
+            default:
+              instructionCounter++;
+              break;          
+          }
+          break;
+          
+      // Halt—i.e., the program has completed its task.
+      default: 
+        instructionCounter = memory.size() + 1;
+        std::cout << "*** Simpletron execution terminated ***\n";
+        break;          
+    } 
+  }
 }
 
