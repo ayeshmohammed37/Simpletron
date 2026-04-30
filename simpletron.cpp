1d0c67dadeab272088e4d9c6afcbd46fdbb31e20
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-04-30 11:38:42 +0300

    SML-2

--- simpletron.cpp
+++ simpletron.cpp
@@ -25,12 +25,9 @@ int main() {
     halt = 43,
   };
 
-  int accumulator{0};
-  int operand{0};
-  int location{0};
   int word{0};
   std::vector<int> memory(100, 0);
-
+  
   int cnt{0};
   std::println("Enter instructions or (-10000) to end the program");
 
@@ -41,12 +38,72 @@ int main() {
     if (word == -10000) {
       break;
     }
-
+    
     memory.at(cnt++) = std::abs(word);
   }
+  
+  int accumulator{0};
+  // int operand{memory.at(0) % 100};
+  // OperationCodes operation{memory.at(0) / 100};
 
-  for (int i{0}; i < cnt; i++) {
-    operand = memory.at(i) % 100;
+  int i{0};
+  while(i < 100) {
+    int operand{memory.at(i) % 100};
+    OperationCodes operation{memory.at(i) / 100};
 
+    switch (operation) {
+      case OperationCodes::read :
+        std::cout << "?";
+        std::cin >> memory.at(operand);
+        i++;
+        break;
+
+      case OperationCodes::write :
+        std::println("{}", memory.at(operand));
+        i++;
+        break;
+      
+      case OperationCodes::load :
+        accumulator = memory.at(operand);
+        i++;
+        break;
+
+      case OperationCodes::store :
+        memory.at(operand) = accumulator;
+        i++;
+        break;
+
+      case OperationCodes::add :
+        accumulator += memory.at(operand);
+        i++;
+        break;
+
+      case OperationCodes::subtract :
+        accumulator -= memory.at(operand);
+        i++;
+        break;
+
+      case OperationCodes::divide :
+        accumulator /= memory.at(operand);
+        i++;
+        break;
+        
+      case OperationCodes::multiply :
+        accumulator *= memory.at(operand);
+        i++;
+        break;
+        
+      case OperationCodes::branch :
+      
+      case OperationCodes::branchNeg :
+      
+      case OperationCodes::branchZero :
+      
+      case OperationCodes::halt :
+      
+      default:
+    }
+  
   }
+  
 }
