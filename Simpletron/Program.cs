458255bf7490500875028d63d61c713e9516ff7d
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-07-20 16:23:03 +0300

    move OperationCodes to OperationCodes.cs

--- Simpletron/Program.cs
+++ Simpletron/Program.cs
@@ -1,30 +1,7 @@
 ﻿namespace Simpletron;
 
-class Program
+partial class Program
 {
-    enum OperationCodes
-    {
-        // Input/output operations:
-        read = 10,
-        write = 11,
-
-        // Load/store operations:
-        load = 20,
-        store = 21,
-
-        // Arithmetic operations:
-        add = 30,
-        subtract = 31,
-        divide = 32,
-        multiply = 33,
-
-        // Transfer-of-control operations:
-        branch = 40,
-        branchNeg = 41,
-        branchZero = 42,
-        halt = 43
-    }
-
 
     static void Main(string[] args)
     {
