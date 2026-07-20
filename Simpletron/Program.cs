8b32c668a44f6a981c6501994adeaa6eb205ff5b
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-07-20 13:42:31 +0300

    get sml instruction to the memory

--- Simpletron/Program.cs
+++ Simpletron/Program.cs
@@ -8,5 +8,25 @@ class Program
         int[] memory = new int[100];
         // special register to store information before simpletron uses it in calculations
         int accumulator = 0;
+
+        // load sml instructions to the memory
+        int i = 0;
+        int instruction = 0;
+
+        Console.WriteLine("Enter instructions within [-9999..9999].\n Enter any value out of the range to exit");
+        while (i < 100)
+        {
+            Console.Write(">");
+            instruction = int.Parse(Console.ReadLine());
+
+            if (instruction >= -9999 && instruction <= 9999)
+            {
+                memory[i++] = instruction;
+            }
+            else
+            {
+                i = 101;
+            }
+        }
     }
 }
