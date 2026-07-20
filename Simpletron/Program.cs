f685cfc4765ab44c2b2f8e22b9119d98448fad98
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-07-20 16:42:17 +0300

    Refactor: LoadSML(), ExecuteSML()

--- Simpletron/Program.cs
+++ Simpletron/Program.cs
@@ -3,13 +3,8 @@
 partial class Program
 {
 
-    static void Main(string[] args)
+    static void LoadSML(int[] mem)
     {
-        // 100-word memory for sml instructions
-        int[] memory = new int[100];
-        // special register to store information before simpletron uses it in calculations
-        int accumulator = 0;
-
         // load sml instructions to the memory
         int i = 0;
         int instruction = 0;
@@ -23,7 +18,7 @@ partial class Program
             // check if the instruction withing the range
             if (instruction >= -9999 && instruction <= 9999)
             {
-                memory[i++] = instruction;
+                mem[i++] = instruction;
             }
             else
             {
@@ -31,9 +26,11 @@ partial class Program
                 i = 101;
             }
         }
+    }
 
-        // Execute sml program that loaded in memory
-        int j = 0;
+    static void ExecuteSML(int[] memory, ref int accumulator)
+    {
+         int j = 0;
         while (j < 100)
         {
             int ins = memory[j];
@@ -91,6 +88,21 @@ partial class Program
             }
             
         }
+    }
+
+    static void Main(string[] args)
+    {
+        // 100-word memory for sml instructions
+        int[] memory = new int[100];
+        // special register to store information before simpletron uses it in calculations
+        int accumulator = 0;
+
+        // load sml instructions to the memory        
+        LoadSML(memory);
+
+        // Execute sml program that loaded in memory
+        ExecuteSML(memory, ref accumulator);
+        
 
     }
 }
