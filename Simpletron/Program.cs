14a4a35351ec5bea0270b16e55c5d187ffa5a553
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-07-20 13:32:03 +0300

    initialize accumulator and 100-word memory

--- Simpletron/Program.cs
+++ Simpletron/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // 100-word memory for sml instructions
+        int[] memory = new int[100];
+        // special register to store information before simpletron uses it in calculations
+        int accumulator = 0;
     }
 }
