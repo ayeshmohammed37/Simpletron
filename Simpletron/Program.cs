76c5c08c995f79c55eb3c54a4a80a24afd052e65
Ayesh <ayeshmohammed37@gmail.com>
2026-09-20 13:59:47 +0300

    Create SimpletronSimulator class, Modifiy main so start simulator.Run()

--- Simpletron/Program.cs
+++ Simpletron/Program.cs
@@ -132,21 +132,6 @@ class Program
 
     static void Main(string[] args)
     {
-         // 100-word memory for sml instructions
-        int[] memory = new int[100];
-        // special register to store information before simpletron uses it in calculations
-        int accumulator = default;
-        // store the number of the memory location (00 to 99) containing the instruction being performed
-        int instructionCounter = default;
-        // store the operation currently being performed (the instruction word’s left two digits)
-        OperationCode operationCode = default;
-        // store the number of the memory location on which the current instruction operates.
-        int operand = default;
-        // the next instruction to be performed from memory 
-        int instructionRegister = default;
-
-
-        // Welcome to Simpletron
         string welcomeMsg =
             "***            Welcome to Simpletron          ***\n" + 
             "***                                           ***\n" + 
@@ -157,17 +142,10 @@ class Program
             "*** Type the sentinel -99999 to stop entering ***\n" + 
             "*** your program.                             ***";
 
-        Console.WriteLine(welcomeMsg);       
+        Console.WriteLine(welcomeMsg);
 
-        // 1- load sml instructions to the memory
-        Load(ref instructionCounter, ref instructionRegister, memory);
-
-        // 2- Execute sml program that loaded in memory
-        Execute(ref instructionCounter, ref instructionRegister, ref operand, ref operationCode, ref accumulator, memory);
-        
-        // 3- prints the name and contents of each register and the complete contents of all 100 memory locations. dump()
-        Dump(in accumulator, in instructionCounter, in instructionRegister, in operationCode, in operand, memory);
-        
+        SimpletronSimulator simulator = new SimpletronSimulator();
+        simulator.Run();        
     }
 
 }
---
+++ Simpletron/SimpletronSimulator.cs
@@ -0,0 +1,42 @@
+namespace Simpletron;
+
+public partial class SimpletronSimulator
+{
+    // 100-word memory for sml instructions
+    private int[] memory = new int[100];
+    // special register to store information before simpletron uses it in calculations
+    private int accumulator = default;
+    // store the number of the memory location (00 to 99) containing the instruction being performed
+    private int instructionCounter = default;
+    // store the operation currently being performed (the instruction word’s left two digits)
+    private OperationCode operationCode = default;
+    // store the number of the memory location on which the current instruction operates.
+    private int operand = default;
+    // the next instruction to be performed from memory 
+    private int instructionRegister = default;
+
+    public void Run()
+    {
+        // 1- load sml instructions to the memory
+        Load();
+        // 2- Execute sml program that loaded in memory
+        Execute();
+        // 3- prints the name and contents of each register and the complete contents of all 100 memory locations. dump()
+        Dump();
+    }
+
+    private void Load()
+    {
+        
+    }
+
+    private void Execute()
+    {
+        
+    }
+
+    private void Dump()
+    {
+        
+    }
+}
